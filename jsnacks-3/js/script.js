888bb1fa7ae9cfc22ade400e1f379898fab5f2cc
Mattia <matpolli93@gmail.com>
2021-03-02 12:12:49 +0100

    Added third exercise

---
+++ jsnacks-3/js/script.js
@@ -0,0 +1,16 @@
+// To print the sum of 10 numbers inserted by the user
+// Assigning variables
+var sum = 0;
+var i;
+
+// Calculating
+userNumber = 0;
+// Asking the user to insert numbers
+var userNumber = parseInt(alert("Hello, please insert a number in the following dialogs:"));
+for (var i= 0; i < 10; i++) {
+    // Adding number to the sum
+    sum += parseInt(prompt(i + 1 + "° number: "));
+}
+
+// Display
+console.log("The sum is " + sum);
