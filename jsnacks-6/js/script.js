163e64bea5ac36d5af16a11447feeec896bb2cb6
Mattia <matpolli93@gmail.com>
2021-03-03 12:11:53 +0100

    Sixth exercise improved

--- jsnacks-6/js/script.js
+++ jsnacks-6/js/script.js
@@ -1,8 +1,10 @@
 // Ask the user a number, then print the cube of all the previous integer numbers and the user's number
-// Assigning variable
-var userNumber = parseInt(prompt("Hello, please write a number!"));
+// Assigning variable and asking the user
+do {
+    var userNumber = parseInt(prompt("Hello, please write a number!"));
+} while (isNaN(userNumber));
 
 // Calculating
 for (var i = 1; i <= userNumber; i++) {
-    alert(Math.pow(i, 3));
+    console.log(Math.pow(i, 3));
 }
